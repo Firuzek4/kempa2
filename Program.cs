56fa700d9e7b0d928d1bdccb75e3f091b0c583c8
Firuzek4 <szymon.firuzek@op.pl>
2019-02-09 12:24:46 +0100

    init commit

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("1");
+            Console.WriteLine("2");
         }
+        public void test() { }
     }
 }

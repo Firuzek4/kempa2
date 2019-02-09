8a3505b6f87db5d8dd57abf29b546f50964d5b9c
Firuzek4 <szymon.firuzek@op.pl>
2019-02-09 12:39:13 +0100

    init commit

--- Program.cs
+++ Program.cs
@@ -10,15 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-            Console.WriteLine("5");
-            Console.WriteLine("6");
+            Program program = new Program();
+            program.zad1();
+            program.zad2();
+            program.zad3();
         }
-        public void test1() { }
-        public void test2() { }
-        public void test3() { }
+        public void zad1() {
+            Console.WriteLine("Zad 1");
+
+        }
+        public void zad2() {
+            Console.WriteLine("Zad 2");
+        }
+        public void zad3() {
+            Console.WriteLine("Zad 3");
+        }
     }
 }
